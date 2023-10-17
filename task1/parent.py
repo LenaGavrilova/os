7d7a19db784ae511b07dc8e1196d09a0b1759efe
LenaGavrilova <lena.gavrilova2408@mail.ru>
2023-10-17 17:07:58 +0300

    do task 1

---
+++ task1/parent.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import os
+import sys
+import random
+
+n = int(sys.argv[1])
+
+ch = n
+while ch > 0:
+    child = os.fork()
+
+    if child > 0:
+        print(f"Parent [{os.getpid()}]: I ran children process with PID {child}")
+    else:
+        os.execl("./child.py", "./child.py", str(random.randint(3, 15)))
+
+    if child > 0:
+        ch -= 1
+
+ch = n
+while ch > 0:
+    ch_pid = os.wait()
+    status = os.wait()
+    if status != 0:
+        child = os.fork()
+
+        if child > 0:
+            print(f"Parent [{os.getpid()}]: I ran children process with PID {child}")
+        else:
+            os.execl("./child.py", "./child.py", str(random.randint(3, 15)))
+    else:
+        print(f"   Parent [{os.getpid()}]: Child with PID {ch_pid} terminated. Exit Status {status}")
+        ch -= 1
+
+os._exit(os.EX_OK)
