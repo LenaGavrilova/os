b92f3c622a6750c2beac83fde00e5d54961e39e9
Lena <90480521+LenaGavrilova@users.noreply.github.com>
2023-10-21 11:57:16 +0300

    Update parent.py

--- task1/parent.py
+++ task1/parent.py
@@ -7,10 +7,12 @@ n = int(sys.argv[1])
 
 ch = n
 while ch > 0:
+    
     child = os.fork()
 
     if child > 0:
-        print(f"Parent [{os.getpid()}]: I ran children process with PID {child}")
+        print("Parent [{os.getpid()}]: I ran children process with PID {child}")
+        
     else:
         os.execl("./child.py", "./child.py", str(random.randint(3, 15)))
 
@@ -18,18 +20,21 @@ while ch > 0:
         ch -= 1
 
 ch = n
+
 while ch > 0:
     ch_pid = os.wait()
     status = os.wait()
+    
     if status != 0:
         child = os.fork()
 
         if child > 0:
-            print(f"Parent [{os.getpid()}]: I ran children process with PID {child}")
+            print("Parent [{os.getpid()}]: I ran children process with PID {child}")
+            
         else:
             os.execl("./child.py", "./child.py", str(random.randint(3, 15)))
     else:
-        print(f"   Parent [{os.getpid()}]: Child with PID {ch_pid} terminated. Exit Status {status}")
+        print("Parent [{os.getpid()}]: Child with PID {ch_pid} terminated. Exit Status {status}")
         ch -= 1
 
 os._exit(os.EX_OK)
