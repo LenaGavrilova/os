31381563d8dd086094f3771c9901020390ab0c8a
Lena <90480521+LenaGavrilova@users.noreply.github.com>
2023-10-21 11:56:01 +0300

    Update child.py

--- task1/child.py
+++ task1/child.py
@@ -7,6 +7,7 @@ import random
 print("Сhild [{os.getpid()}]: I am started. My PID {os.getpid()}. Parent PID {os.getppid()}")
 
 s = int(sys.argv[1])
+
 time.sleep(s)
 
 print("Child [{os.getpid()}]: I am ended. PID {os.getpid()}. Parent PID {os.getppid()}")
